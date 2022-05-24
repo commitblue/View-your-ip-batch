b283905432db7594633885312c5cc8f8a1015bdf
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-05-24 20:21:28 +0200

    Added batch
    the main

--- viewurip.bat
+++ viewurip.bat
@@ -1,8 +1,7 @@
 @echo off
 title view ip
 set ip_address_string="IPv4 Address"
-rem Uncomment the following line when using older versions of Windows without IPv6 support (by removing "rem")
-rem set ip_address_string="IP Address"
+
 for /f "usebackq tokens=2 delims=:" %%f in (`ipconfig ^| findstr /c:%ip_address_string%`) do set IP=%%f
 if exist currentipadress goto itexists
 echo Press any key to view IP
